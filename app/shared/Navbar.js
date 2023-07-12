0d6f468fc1b84fb57c46e21037721f12b5bf1cc8
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-07-12 11:43:19 -0400

    Added navbar

---
+++ app/shared/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return(
+        <header className="text-zinc-300 bg-zinc-900">
+            <div className="container mx-auto bg-red-500">
+                hello
+            </div>
+        </header>
+    )
+}
